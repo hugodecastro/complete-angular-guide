e99318291113aad8494ec15fc3c81c30d48fc944
Hugo de Castro <hugo.castro@ibm.com>
2022-03-31 20:48:00 +0000

    using list of recipes

--- shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is Just a Test',
+      'https://www.seriouseats.com/thmb/2OxF-UNLyWo2qMLSc6unSi7yH5o=/1500x1125/filters:fill(auto,1)/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__recipes__images__2016__06__20160623-cubano-roast-pork-sandwich-recipe-19-57695d21e77947538db375d1d30b4bdf.jpg'
+    )
+  ];
 
   constructor() { }
 
