b14f8d1676aeda056450fcdeb4bbfed2667c3f4b
Hugo <hugo.castro@ibm.com>
2024-03-19 12:00:07 -0300

    add lesson comments

--- base-angular-app/src/app/servers/servers.component.ts
+++ base-angular-app/src/app/servers/servers.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // selector: '[app-servers]', // selecting by attribute
+  // selector: '.app-servers', // selecting by class
   templateUrl: './servers.component.html', // need to have at least one template
   // template: '<app-server></app-server><app-server></app-server>' // same result as above, used for shorts inline html
   styleUrls: ['./servers.component.css'],
