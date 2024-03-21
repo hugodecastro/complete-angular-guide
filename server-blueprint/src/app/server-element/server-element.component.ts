bf0eec46fb3487969d1e55a50b9c252e662e7dc6
Hugo <hugo.castro@ibm.com>
2024-03-21 14:22:48 -0300

    add view encapsulation comments

--- server-blueprint/src/app/server-element/server-element.component.ts
+++ server-blueprint/src/app/server-element/server-element.component.ts
@@ -1,10 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { ServerElement } from '../shared/serverElement.model';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated, // Default
+  // encapsulation: ViewEncapsulation.None // Don't add attribute to elements in component and its styles will affect whole DOM
+  // encapsulation: ViewEncapsulation.ShadowDom // Uses Shadow Dome approach
 })
 export class ServerElementComponent implements OnInit {
   //by default all properties can be only accessed by its own class
