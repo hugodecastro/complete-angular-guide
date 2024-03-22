1dc321199cfaad20f7d5988179c9c765213c28cf
Hugo <hugo.castro@ibm.com>
2024-03-22 12:15:19 -0300

    add custom directive to toggle menu

--- shopping-list/src/app/shared/dropdown.directive.ts
+++ shopping-list/src/app/shared/dropdown.directive.ts
@@ -1,8 +1,13 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]',
 })
 export class DropdownDirective {
+  @HostBinding('class.open') isOpen: boolean = false;
+
+  @HostListener('click') toggleOpen(): void {
+    this.isOpen = !this.isOpen;
+  }
   constructor() {}
 }
