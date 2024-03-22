bff3d1cb253d72834b5e21aea2ddafaa7ac776c9
Hugo <hugo.castro@ibm.com>
2024-03-22 11:38:02 -0300

    add lesson comments

--- directive-deep-dive/src/app/better-highlight/better-highlight.directive.ts
+++ directive-deep-dive/src/app/better-highlight/better-highlight.directive.ts
@@ -14,23 +14,26 @@ import {
 export class BetterHighlightDirective implements OnInit {
   @Input() defaultColor: string = 'transparent';
   @Input('appBetterHighlight') highlightColor: string = 'blue';
+  // @HostBinding is a easier approach to Render
+  // HostBinding will access the style property and change background color on the element this directive is (in the example will be on p)
   @HostBinding('style.backgroundColor') backgroundColor: string;
 
   constructor(
     private elRef: ElementRef,
     private renderer: Renderer2
-  ) {}
+  ) {} // render is not limited to the browser and should be used for any DOM manipulations
 
   ngOnInit(): void {
     this.backgroundColor = this.defaultColor;
   }
 
-  @HostListener('mouseenter') mouseOver(eventData: Event) {
+  // @HostListener decorator reacts to any user event and creates an event binding to the DOM event
+  @HostListener('mouseenter') mouseOver(eventData: Event): void {
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
     this.backgroundColor = this.highlightColor;
   }
 
-  @HostListener('mouseleave') mouseLeave(eventData: Event) {
+  @HostListener('mouseleave') mouseLeave(eventData: Event): void {
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
     this.backgroundColor = this.defaultColor;
   }
