3e292d515669627008be933cafdd1d61f3f21428
Hugo <hugo.castro@ibm.com>
2024-03-22 17:06:47 -0300

    linter files

--- active-to-inactive/src/app/active-users/active-users.component.ts
+++ active-to-inactive/src/app/active-users/active-users.component.ts
@@ -4,9 +4,9 @@ import { UserService } from '../user.service';
 @Component({
   selector: 'app-active-users',
   templateUrl: './active-users.component.html',
-  styleUrls: ['./active-users.component.css']
+  styleUrls: ['./active-users.component.css'],
 })
-export class ActiveUsersComponent implements OnInit{
+export class ActiveUsersComponent implements OnInit {
   users: string[];
 
   constructor(private userService: UserService) {}
--- active-to-inactive/src/app/user.service.ts
+++ active-to-inactive/src/app/user.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from "@angular/core";
-import { CounterService } from "./counter.service";
+import { Injectable } from '@angular/core';
+import { CounterService } from './counter.service';
 
 @Injectable()
 export class UserService {
@@ -10,13 +10,13 @@ export class UserService {
 
   setToActive(id: number) {
     this.activeUsers.push(this.inactiveUsers[id]);
-    this.inactiveUsers.splice(id,1);
+    this.inactiveUsers.splice(id, 1);
     this.counterService.incrementInactiveToActive();
-}
+  }
 
-setToInactive(id: number) {
+  setToInactive(id: number) {
     this.inactiveUsers.push(this.activeUsers[id]);
-    this.activeUsers.splice(id,1);
+    this.activeUsers.splice(id, 1);
     this.counterService.incrementActiveToInactive();
   }
 }
