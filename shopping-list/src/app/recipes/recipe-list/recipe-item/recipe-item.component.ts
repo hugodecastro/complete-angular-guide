0b92d48c333941aeea1cb27b1eaeac390bf7d93f
Hugo <hugo.castro@ibm.com>
2024-03-22 09:23:36 -0300

    add navigation to recipe details

--- shopping-list/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ shopping-list/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -9,4 +9,9 @@ import { Recipe } from '../../recipe.model';
 export class RecipeItemComponent {
   // @Input decorator allows to bind property to outside
   @Input() recipe: Recipe;
+  @Output() recipeSelected: EventEmitter<void> = new EventEmitter<void>();
+
+  onSelected(): void {
+    this.recipeSelected.emit();
+  }
 }
