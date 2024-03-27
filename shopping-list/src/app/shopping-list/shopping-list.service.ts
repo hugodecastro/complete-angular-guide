9a6498a659a20ab7b1c0c251186bab288c66aa40
Hugo <hugo.castro@ibm.com>
2024-03-27 11:39:49 -0300

    implement shopping list service

--- shopping-list/src/app/shopping-list/shopping-list.service.ts
+++ shopping-list/src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,26 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.module';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ShoppingListService {
+  ingredientChanged: EventEmitter<Ingredient[]> = new EventEmitter<
+    Ingredient[]
+  >();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+
   constructor() {}
+
+  getIngredients(): Ingredient[] {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient): void {
+    this.ingredients.push(ingredient); // at this point we are pushing to the copy, not to the original
+    this.ingredientChanged.emit(this.ingredients.slice()); // inform the change on ingredient
+  }
 }
